be1996208ca89c9826b73c8498f94c090f0c888e
FW <fraserwatt2005@gmail.com>
2025-02-04 16:44:53 +0000

    Added comments + beginning Stage 2

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
     int x = 1; 
@@ -11,16 +14,21 @@ int main() {
         char userinput[100]; 
         fgets(userinput, sizeof(userinput), stdin); // getting user input
         
+		// Checking for built in shell functions:
+		
+		// Ctrl+d check
         if (feof(stdin)) { // ctrl+d -> exit program
+			printf("\n");
             break;
         }
+		// exit check 
         if (strcmp(userinput, "exit\n") == 0) { exit -> exit program 
             break;
         }
 
         char* tokens[100];
         int token_count = 0;
-        char* token = strtok(result, " < \t | > & ;"); // tokens 
+        char* token = strtok(result, " < \t | > & ;"); // tokens to look for
         while (token != NULL) {
             char *newline = strchr(token, '\n');
             if (newline) {
@@ -30,9 +38,35 @@ int main() {
             token = strtok(NULL, " < \t | > & ;");
         }
         
+		/*
         for (int i = 0; i < token_count; i++) {
             printf("'%s'\n", tokens[i]); // loop through tokens printing them
         }
+		*/
+		
+		// Else - stage 2:
+		
+		pid_t = pid;
+		pid = fork();
+		
+		if(pid<0){ // negative pid -> failure
+			fprintf(stderr, "Fork failed\n"); // Fork failed
+			return 1;
+		}
+		else if(pid==0){ // signifies child process
+			execlp("/bin/ls","ls",NULL); // if fails - not our fault - OS problem
+			// only reached if exec fils - exit child process here:
+		}
+		else{
+			wait(NULL); // Parent waiting for child process to complete
+		}
+		
     }
 	
 }
+
+/* Notes:
+- Dont use any exec() with 'e' in the name -> requires working path
+
+
+*/
